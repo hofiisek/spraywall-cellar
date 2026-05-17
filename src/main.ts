c42b3546861f41322f59491e1a0de2b6f0d176e3
Dominik Hoftych <dominik.hoftych@shipmonk.com>
2026-05-17 22:36:15 +0200

    changed name field title

--- src/main.ts
+++ src/main.ts
@@ -224,7 +224,7 @@ function renderHTML(): void {
           <input
             type="text"
             id="boulder-name"
-            placeholder="Boulder name"
+            placeholder="Name"
             class="w-full px-3 py-2 mb-2 bg-gray-600 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           <select
